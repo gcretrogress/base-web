1a7f61ccea197056443b9eae5a3442ea866c919d
gcretrogress <gcretrogress@yahoo.com>
2017-09-18 22:44:12 -0400

    Changes to run on jetty server

---
+++ src/main/java/base/servlets/HelloWorldServlet.java
@@ -0,0 +1,25 @@
+package base.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+
+   private static final long serialVersionUID = 1L;
+
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse res) 
+        throws ServletException, IOException {
+         
+        String url = req.getRequestURL().toString();
+        String output = "Hello World" + url;
+        res.getOutputStream().write(output.getBytes());
+
+  }
+
+}
+
